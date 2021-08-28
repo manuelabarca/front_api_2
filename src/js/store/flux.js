c675cd871fee36edc3557ba663cdbdcf752aed36
Manuel Labarca <labarca.manu@gmail.com>
2021-08-28 10:01:23 +0000

    Update with examples

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,7 +12,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			api: "https://3000-green-cuckoo-p26wqibt.ws-eu16.gitpod.io",
+			planets: [],
+			isAuthenticate: false,
+			message: ""
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -37,6 +41,65 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
+			},
+			sign_in: (email, password) => {
+				const store = getStore();
+
+				fetch(`${store.api}/login`, {
+					method: "POST",
+					body: JSON.stringify({
+						email: email,
+						password: password
+					}),
+					headers: {
+						"Content-type": "application/json"
+					}
+				})
+					.then(resp => {
+						if (resp.ok) {
+							return resp.json();
+						}
+					})
+					.then(data => {
+						localStorage.setItem("token", data.token);
+						setStore({ isAuthenticate: true });
+					})
+					.catch(error => console.error("[ERROR IN LOGIN]", error));
+			},
+			getAllPlanets: () => {
+				const store = getStore();
+				fetch(`${store.api}/planet`, {
+					method: "GET",
+					headers: {
+						Authorization: `Bearer ${localStorage.getItem("token")}`,
+						"Content-type": "application/json"
+					}
+				})
+					.then(resp => {
+						if (resp.ok) {
+							return resp.json();
+						}
+					})
+					.then(data => setStore({ planets: data }))
+					.catch(error => console.error("[ERROR TO GET PLANETS]", error));
+			},
+			deletePlanet: id => {
+				const store = getStore();
+				fetch(`${store.api}/planet/${id}`, {
+					method: "DELETE",
+					headers: {
+						Authorization: `Bearer ${localStorage.getItem("token")}`,
+						"Content-type": "application/json"
+					}
+				})
+					.then(resp => {
+						if (resp.ok) {
+							return resp.json();
+						}
+					})
+					.then(data => {
+						setStore({ message: data.msg });
+					});
 			}
 		}
 	};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,35 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useContext, useEffect } from "react";
 import "../../styles/home.scss";
+import { Context } from "../store/appContext";
+import { useHistory } from "react-router-dom";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const { store, actions } = useContext(Context);
+
+	const history = useHistory();
+
+	useEffect(
+		() => {
+			if (store.isAuthenticate) {
+				history.push("/demo");
+			}
+		},
+		[store.isAuthenticate]
+	);
+
+	return (
+		<div className="text-center mt-5">
+			<input type="email" placeholder="Ingresar email" onChange={e => setEmail(e.target.value)} value={email} />
+			<input
+				type="password"
+				placeholder="Ingresar contraseña"
+				onChange={e => setPassword(e.target.value)}
+				value={password}
+			/>
+			<button onClick={() => actions.sign_in(email, password)}>Ingresar</button>
+		</div>
+	);
+};
